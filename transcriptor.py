fe270c847f9c230f908ddcdae36749cfb976689e
GM-11 <mathuyrgopal@gmail.com>
2025-01-26 20:57:21 +0530

    removed langchain-community dependancy

--- transcriptor.py
+++ transcriptor.py
@@ -1,5 +1,4 @@
 import streamlit as st
-from langchain_community.document_loaders import YoutubeLoader
 from googletrans import Translator
 from youtube_transcript_api import YouTubeTranscriptApi
 from gtts import gTTS
